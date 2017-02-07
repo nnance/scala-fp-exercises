a0ed813316b827a9ded5f9c24a1f65089b66cc3b
Nick Nance <nance.nick@gmail.com>
2017-02-07 08:44:17 -0800

    dep injection example (cont)

--- src/main/scala/exercises/DocumentRepository.scala
+++ src/main/scala/exercises/DocumentRepository.scala
@@ -4,28 +4,68 @@ package exercises
   * Created by nicknance on 2/6/17.
   */
 
-trait UserDomain {
+object UserDomain {
   type UserID = String
+  type UserRepository = List[User]
 
   case class User(id: UserID, name: String)
 }
 
-trait UserDB {
-  import exercises.UserDomain._
+object UserDB {
+  import UserDomain._
 
-  def findOne(userID: UserID): User
+  type FindOne = UserID => Option[User]
+}
+
+case class UserMemoryDB(users: List[UserDomain.User]) {
+  import UserDomain._
+
+  def findOne(userId: UserID): Option[User] =
+    users.find(u => u.id == userId)
+}
+
+object FolderDomain {
+  type Location = String
+  type FileStream = String
+
+  case class Folder(path: String)
+}
+
+trait FolderDB {
+  import FolderDomain._
+
+  def FindOne: Location => Folder
+}
+
+trait FileStorage {
+  import UserDomain._
+  import FolderDomain._
+
+  def canWrite: (User, Folder) => Boolean
+  def save: (FileStream, User, Folder) => Boolean
 }
 
 object DocumentRepository {
-  import exercises.UserDB._
+  import UserDomain._
+  import FolderDomain._
+  import UserDB._
 
-  def storeDocument(userDB: String, folderLocation: String): Boolean = {
-    val user = userDB.findOne(userID)
-    val folder = folderDB.findOne(folderLocation)
-    if (!userAccess.canWrite(user, folder)) {
-      fileStorage.save(fileStream, user, folder)
-    } else {
-      false
-    }
+  def storeDocument(findOne: FindOne, folderDB: FolderDB, fileStorage: FileStorage,
+                    userID: UserID, folderLocation: Location, fileStream: FileStream): Boolean = {
+    findOne(userID).exists(u => {
+      val folder = folderDB.findOne(folderLocation)
+      if (!fileStorage.canWrite(u, folder)) {
+        fileStorage.save(fileStream, u, folder)
+      } else {
+        false
+      }
+    })
+  }
+}
+
+object DocumentTest {
+  def main(args: Array[String]): Unit = {
+
+    System.out.println("")
   }
 }
